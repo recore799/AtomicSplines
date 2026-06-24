bebd94b81225db521463f366c67d4bf82c511909
Rafa <recore799@gmail.com>
2026-06-24 17:34:05 -0600

    trash

--- docs/auto/main.el
+++ docs/auto/main.el
@@ -5,10 +5,22 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("book" "12pt" "a4paper" "oneside" "spanish")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("babel" "es-tabla" "es-nodecimaldot" "spanish" "es-noquoting") ("mathtools" "") ("amssymb" "") ("amsthm" "") ("physics" "") ("bm" "") ("caption" "") ("graphicx" "") ("booktabs" "") ("array" "") ("tabularx" "") ("ragged2e" "") ("enumitem" "") ("xcolor" "") ("listings" "") ("inconsolata" "") ("mdframed" "") ("thmtools" "") ("hyperref" "colorlinks=true" "linkcolor=blue" "citecolor=red" "urlcolor=teal") ("cleveref" "nameinlink" "noabbrev") ("biblatex" "backend=biber" "style=apa" "sorting=nyt") ("geometry" "")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "config/preamble"
-    "chapters/framework"
+    "chapters/marco-teorico"
     "book"
     "bk12"))
  :latex)
---
+++ docs/config/auto/preamble.el
@@ -0,0 +1,67 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "preamble"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("geometry" "") ("fontenc" "T1") ("babel" "es-tabla" "es-nodecimaldot" "spanish" "es-noquoting") ("mathtools" "") ("amssymb" "") ("amsthm" "") ("physics" "") ("bm" "") ("caption" "") ("graphicx" "") ("booktabs" "") ("array" "") ("tabularx" "") ("ragged2e" "") ("enumitem" "") ("todonotes" "colorinlistoftodos" "textsize=tiny") ("xcolor" "") ("listings" "") ("inconsolata" "") ("mdframed" "") ("thmtools" "") ("hyperref" "colorlinks=true" "linkcolor=blue" "citecolor=red" "urlcolor=teal") ("cleveref" "nameinlink" "noabbrev") ("biblatex" "backend=biber" "style=apa" "sorting=nyt")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "fontenc"
+    "babel"
+    "mathtools"
+    "amssymb"
+    "amsthm"
+    "physics"
+    "bm"
+    "caption"
+    "graphicx"
+    "booktabs"
+    "array"
+    "tabularx"
+    "ragged2e"
+    "enumitem"
+    "todonotes"
+    "xcolor"
+    "listings"
+    "inconsolata"
+    "mdframed"
+    "thmtools"
+    "hyperref"
+    "cleveref"
+    "biblatex")
+   (TeX-add-symbols
+    '("tabularxcolumn" 1))
+   (LaTeX-add-environments
+    '("proof" LaTeX-env-args ["argument"] 0))
+   (LaTeX-add-bibliographies
+    "bib/bibliografia")
+   (LaTeX-add-xcolor-definecolors
+    "gruvbg"
+    "gruvfg"
+    "gruvred"
+    "gruvgreen"
+    "gruvgray"
+    "gruvblue"
+    "gruvyellow")
+   (LaTeX-add-thmtools-declaretheoremstyles
+    "miestilo-plano"
+    "miestilo-caja")
+   (LaTeX-add-thmtools-declaretheorems
+    "theorem"
+    "lemma"
+    "proposition"
+    "corollary"
+    "definition"
+    "example"))
+ :latex)
+
